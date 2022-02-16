9538ec4f4cf14154f2238e2569c408be03e1f1af
nabendu82 <nabendu.biswas@gmail.com>
2022-02-16 20:10:26 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AccountService } from './accounts.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  accounts: {name: string, status: string}[] = [];
+
+  constructor(private accountsService: AccountService){}
+
+  ngOninit(){
+    this.accounts = this.accountsService.accounts;
+  }
+
+}
---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,18 @@
+import { Component} from '@angular/core';
+import { AccountService } from '../accounts.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+})
+
+export class NewAccountComponent {
+  constructor(private accountsService: AccountService) {
+    this.accountsService.statusUpdated.subscribe((status: string) => alert('New Status: ' + status));
+  }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountsService.addAccount(accountName, accountStatus);
+  }
+}
